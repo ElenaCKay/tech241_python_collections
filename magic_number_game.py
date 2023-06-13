cb477123685a5eae20ca2df77bd475034963c196
Elena Kay <elenakay1@gmail.com>
2023-06-13 17:50:27 +0100

    added a difficulty feature

--- magic_number_game.py
+++ magic_number_game.py
@@ -2,15 +2,27 @@
 
 import random
 
-magic_number = random.randint(1, 20)
-guess_count = 0
-
 user_name = input("What is your name? ")
 print(f"Hello {user_name}! This is the magic number game. If you would like to quit at any time just type quit")
+level = input("You get three go's to find the magic number! Pick a level: easy, medium, hard, insane ")
+
+biggest_num = 20
+
+if level == "easy":
+    biggest_num = 10
+if level == "medium":
+    biggest_num = 20
+if level == "hard":
+    biggest_num = 50
+if level == "insane":
+    biggest_num = 100
+
+magic_number = random.randint(1, int(biggest_num))
+guess_count = 0
 
 while guess_count <= 3:
     try:
-        user_guess = input("Pick a number between 1 and 20: ")
+        user_guess = input(f"Pick a number between 1 and {biggest_num}: ")
         guess_count += 1
         if user_guess == "quit":
             break
@@ -19,7 +31,7 @@ while guess_count <= 3:
             print(f"You did it! You guessed the magic number {magic_number}!")
             break
         if guess_count == 3:
-            try_again = input(f"You ran out of guesses! The magic number was {magic_number}. Try again...")
+            print(f"You ran out of guesses! The magic number was {magic_number}. Try again...")
             break
         elif user_number < magic_number:
             print("That number is too small... ")
