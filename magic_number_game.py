43ecfe231601accc5ed1d080fbb8f681c3930f5b
Elena Kay <elenakay1@gmail.com>
2023-06-13 17:24:58 +0100

    added a quit feature

--- magic_number_game.py
+++ magic_number_game.py
@@ -4,21 +4,24 @@ import random
 
 magic_number = random.randint(1, 20)
 guess_count = 0
+user_quit = False
 
-print("Hello! This is the magic number game.")
+print("Hello! This is the magic number game. If you would like to quit at any time just type quit")
 
 while guess_count <= 3:
     try:
-        user_guess = int(input("Pick a number between 1 and 20: "))
+        user_guess = input("Pick a number between 1 and 20: ")
         guess_count += 1
-
-        if user_guess == magic_number:
+        if user_guess == "quit":
+            break
+        user_number = int(user_guess)
+        if user_number == magic_number:
             print(f"You did it! You guessed the magic number {magic_number}!")
             break
         if guess_count == 3:
             print(f"You ran out of guesses! The magic number was {magic_number}. Try again...")
             break
-        elif user_guess < magic_number:
+        elif user_number < magic_number:
             print("That number is too small... ")
         else:
             print("That number is too big... ")
